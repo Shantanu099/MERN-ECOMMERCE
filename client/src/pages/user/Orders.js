b2b67cf72b1c3b082e6b4423bf5264f6f593bf71
Shantanu Baruli <141471916+Shantanu099@users.noreply.github.com>
2024-04-22 01:53:25 +0530

    minor changes

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -61,7 +61,9 @@ const Orders = () => {
                       <div className="row mb-2 p-3 card flex-row" key={p._id}>
                         <div className="col-md-4">
                           <img
-                            src={`/api/v1/product/product-photo/${p._id}`}
+                            src={
+                              baseUrl + `/api/v1/product/product-photo/${p._id}`
+                            }
                             className="card-img-top"
                             alt={p.name}
                             width="300px"
